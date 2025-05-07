8e9f65011562d074d9e99396d707f3e4b54e1bdb
Arjav <arjavbadjate1709@gmail.com>
2025-05-07 21:37:05 +0530

    fff

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: ["/api/cart", "/api/products"], // Add public routes here
+});
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*|favicon.ico).*)", // protects everything except public/static/_next
+    "/((?!_next|.*\\..*|favicon.ico).*)",
   ],
 };
